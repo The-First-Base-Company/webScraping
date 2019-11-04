d9c2428fd1f9fdc140ebcfa7f1a5c27a138a1feb
santiago gonzález angeles <42563977+sglza@users.noreply.github.com>
2019-11-03 19:51:33 -0600

    player DB done

--- insert.py
+++ insert.py
@@ -8,23 +8,106 @@ def playerHitting(db, *args):
     cursor = db.cursor()
     mylist = list(args)
 
-    insertPlayer = "INSERT INTO jugador VALUES (NULL, \"%s\", \"%s\", \"%s\");" % (
+    #id = inicial + apellido + equipo
+    #ej. KHendricksCHC
+    id = mylist[0].replace(",", "").split()
+    id = id[1] + id[0] + mylist[1]
+
+    insertPlayer = "INSERT INTO jugador VALUES (\"%s\", \"%s\", \"%s\", \"%s\");" % (
+        id,
         mylist[0],
         mylist[1],
         mylist[2],
     )
 
-    insertPlayerStats = "INSERT INTO jugadorstats VALUES (NULL, \"HITTING\", %s, %s, %s, %s, %s, %s, NULL, NULL, NULL, NULL, NULL, NULL, NULL);" % (
+    insertPlayerStats = "INSERT INTO jugadorstats VALUES (\"%s\", \"HITTING\", %s, %s, %s, %s, %s, %s, %s, NULL, NULL, NULL, NULL, NULL, NULL, NULL);" % (
+        id,
         mylist[3],
         mylist[4],
         mylist[5],
         mylist[6],
         mylist[7],
         mylist[8],
+        mylist[9],
     )
 
-    #print(insertPlayer)
-    #print(insertPlayerStats)
+    try:
+        cursor.execute(insertPlayer)
+        db.commit()
+        cursor.execute(insertPlayerStats)
+        db.commit()
+    except:
+        print("Error: Unable to insert data")
+
+
+#player pitching
+def playerPitching(db, *args):
+
+    # prepare a cursor object using cursor() method
+    cursor = db.cursor()
+    mylist = list(args)
+
+    #id = inicial + apellido + equipo
+    #ej. JBeeksBOS
+    id = mylist[0].replace(",", "").split()
+    id = id[1] + id[0] + mylist[1]
+
+    insertPlayer = "INSERT INTO jugador VALUES (\"%s\", \"%s\", \"%s\", \"P\");" % (
+        id,
+        mylist[0],
+        mylist[1],
+    )
+
+    insertPlayerStats = "INSERT INTO jugadorstats VALUES (\"%s\", \"PITCHING\", %s, %s, %s, NULL, %s, %s, %s, %s, %s, NULL, NULL, NULL, NULL, NULL);" % (
+        id,
+        mylist[2],
+        mylist[3],
+        mylist[4],
+        mylist[5],
+        mylist[6],
+        mylist[7],
+        mylist[8],
+        mylist[9],
+    )
+
+    try:
+        cursor.execute(insertPlayer)
+        db.commit()
+        cursor.execute(insertPlayerStats)
+        db.commit()
+    except:
+        print("Error: Unable to insert data")
+
+
+#player fielding
+def playerFielding(db, *args):
+
+    # prepare a cursor object using cursor() method
+    cursor = db.cursor()
+    mylist = list(args)
+
+    #id = inicial + apellido + equipo
+    #ej. JBeeksBOS
+    id = mylist[0].replace(",", "").split()
+    id = id[1] + id[0] + mylist[1]
+
+    insertPlayer = "INSERT INTO jugador VALUES (\"%s\", \"%s\", \"%s\", \"%s\");" % (
+        id,
+        mylist[0],
+        mylist[1],
+        mylist[2],
+    )
+
+    insertPlayerStats = "INSERT INTO jugadorstats VALUES (\"%s\", \"FIELDING\", %s, %s, NULL, NULL, NULL, NULL, NULL, NULL, NULL, %s, %s, %s, %s, %s);" % (
+        id,
+        mylist[3],
+        mylist[4],
+        mylist[5],
+        mylist[6],
+        mylist[7],
+        mylist[8],
+        mylist[9],
+    )
 
     try:
         cursor.execute(insertPlayer)
